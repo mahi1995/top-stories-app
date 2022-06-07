b94f696d8f7788a2498c7fd8886c6077f7613835
mahi1995 <77606824+mahi1995@users.noreply.github.com>
2022-06-06 17:04:14 -0700

    Create New York Top Stories project

---
+++ NewYorkTimesTopStories/Shared/NewYorkTimesTopStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewYorkTimesTopStoriesApp.swift
+//  Shared
+//
+//  Created by ZALORA_USER on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewYorkTimesTopStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
