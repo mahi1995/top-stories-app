899b86d238b56dbc3cc7b17945fa9ab99874e451
Mahika Hosanee <mahika.hosanee@zalora.com>
2022-06-07 21:35:55 -0700

    Add navigation bar and collectionView on Home page

---
+++ NewYorkTimesTopStories/Shared/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  NewYorkTimesTopStories
+//
+//  Created by Mahika on 06/06/2022.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
